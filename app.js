18c9b37143235fe7c7dd98d554cc2d5392f1f43e
Alex <safronov.a@brightfit.ru>
2021-10-24 20:38:47 +0500

    deploy

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const http = require('http')
 const socketEvents = require('./socket')
 const httpServer = http.createServer(app)
+
+const PORT = process.env.PORT || 3000
 
 const io = require('socket.io')(httpServer, {
     cors: {
@@ -13,9 +16,12 @@ const io = require('socket.io')(httpServer, {
 
 socketEvents(io)
 
-const PORT = process.env.PORT || 3000
-
 app.use(cors('*'))
+app.use(express.static(path.join(__dirname, 'client')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/index.html'))
+})
 
 httpServer.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
